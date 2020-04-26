102476ebe755639a3a2a2c59dcbda2edb87c21a3
Patrick Rivera <patrickrivera@Patricks-MacBook-Pro.local>
2020-04-26 15:08:22 -0700

    Create compile script and config file

---
+++ config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    infuraEndpoint: process.env.INFURA_ENDPOINT,
+    privateKey: process.env.PRIVATE_KEY,
+    zeroAddress: "0x0000000000000000000000000000000000000000",
+    gasConfig: {
+        gasPrice: "5000000000",
+        gasLimit: "1500000"
+    }
+}
---
+++ scripts/compile-smart-contract.js
@@ -0,0 +1,54 @@
+const solc = require('solc');
+const fs = require('fs');
+
+const MyContractName = 'CErc20Supplier';
+const myContractFileName = `${MyContractName}.sol`;
+const myContractFilePath = `./contracts/${myContractFileName}`;
+const myContractCode = fs.readFileSync(myContractFilePath).toString();
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    [myContractFileName]: {
+      content: myContractCode
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+if (output.errors) {
+  let allWarnings = true;
+  output.errors.forEach((potentiallyAnError) => {
+    if (potentiallyAnError.type !== 'Warning') allWarnings = false;
+  });
+
+  console.error(output.errors);
+
+  if (!allWarnings) {
+    console.error('BUILD FAILED, EXITING.');
+    process.exit(1);
+  }
+}
+
+const myContractOutput = output.contracts[myContractFileName][MyContractName];
+const bytecode = myContractOutput.evm.bytecode.object;
+const abi = myContractOutput.abi;
+
+fs.mkdir('./.build/', { recursive: true }, (err) => {
+  if (err) {
+    console.error('Error creating a build directory', err);
+    console.error('BUILD FAILED, EXITING.');
+    process.exit(1);
+  }
+});
+
+fs.writeFileSync('./.build/bytecode.json', JSON.stringify({ bytecode }));
+fs.writeFileSync(`./.build/${MyContractName}Abi.json`, JSON.stringify(abi));
