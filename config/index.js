22ef9167f07f415699cb4ce44bdd5623b9863104
Patrick Rivera <patrickrivera@Patricks-MacBook-Pro.local>
2020-04-26 15:12:36 -0700

    Deployed smart contract

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ module.exports = {
     privateKey: process.env.PRIVATE_KEY,
     zeroAddress: "0x0000000000000000000000000000000000000000",
     gasConfig: {
-        gasPrice: "5000000000",
-        gasLimit: "1500000"
+        gasPrice: "3000000000",
+        gasLimit: "1000000"
     }
 }
---
+++ scripts/deploy-smart-contract.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const { bytecode } = require('../.build/bytecode.json');
+const abi = require('../.build/CErc20SupplierAbi.json');
+const Web3 = require('web3');
+const { infuraEndpoint, privateKey, zeroAddress, gasConfig } = require("../config");
+
+const web3 = new Web3(infuraEndpoint);
+
+(async function() {
+    web3.eth.accounts.wallet.add(`0x${privateKey}`);
+
+    const myWalletAddress = web3.eth.accounts.wallet[0].address;
+
+    const cErc20SupplierContract = new web3.eth.Contract(abi);
+
+    const result = await cErc20SupplierContract.deploy({
+        data: `0x${bytecode}`,
+    }).send({
+        from: myWalletAddress,
+        to: zeroAddress,
+        ...gasConfig   
+    })
+
+    console.log(result);
+
+    console.log("Successfully deployed contract!")
+})();
